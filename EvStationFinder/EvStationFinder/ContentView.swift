af19280a0b2ba9146249f295b64e84e48ae00817
elyndiaye <elyexplore003@hotmail.com>
2025-07-06 01:52:06 -0300

    feat: improvements and handle error

--- EvStationFinder/EvStationFinder/ContentView.swift
+++ EvStationFinder/EvStationFinder/ContentView.swift
@@ -9,23 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var viewModel: HomeViewModel
-    @State private var zipCode: String = ""
     
     var body: some View {
         NavigationView {
             ZStack {
                 VStack {
                     HStack {
-                        TextField("Type the U.S zip Code", text: $zipCode)
+                        TextField("Type the U.S zip Code", text: $viewModel.zipCode)
                             .padding(.leading)
                             .textFieldStyle(RoundedBorderTextFieldStyle())
                             .keyboardType(.numberPad)
-
-                        Button(action: {
-                            print("zip code: \(zipCode)")
+                        
+                        Button(action: {                        
                             Task {
-                                               await viewModel.loadStations(zipCode: zipCode)
-                                           }
+                                await viewModel.loadStations()
+                            }
                             
                         }) {
                             Image(systemName: "magnifyingglass")
@@ -33,27 +31,35 @@ struct ContentView: View {
                         .padding(.trailing)
                         .buttonStyle(.borderedProminent)
                     }
-                    .padding()
-
-                    List(viewModel.stations) { station in
-                        VStack(alignment: .leading, spacing: 4) {
-                            Text(station.station_name).font(.headline)
-                            Text("\(station.street_address), \(station.city) - \(station.state), \(station.zip)")
-.font(.subheadline)
-                        }
-                        .padding()
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .background(Color(.secondarySystemBackground))
-                        .cornerRadius(8)
-                        .shadow(color: .black.opacity(0.05), radius: 2, x: 0, y: 1)
-                        .padding(.vertical, 4)
+                    .padding(.horizontal)
+                    if viewModel.stations.isEmpty {
+                        Spacer()
+                        Text("Nenhuma estação carregada")
+                            .foregroundColor(.secondary)
+                        Spacer()
                     }
-                    .listStyle(.plain)
+                    else {
+                        List(viewModel.stations) { station in
+                            VStack(alignment: .leading, spacing: 4) {
+                                Text(station.station_name).font(.headline)
+                                Text("\(station.street_address), \(station.city) - \(station.state), \(station.zip)")
+                                    .font(.subheadline)
+                            }
+                            .padding()
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .background(Color(.secondarySystemBackground))
+                            .cornerRadius(8)
+                            .shadow(color: .black.opacity(0.05), radius: 2, x: 0, y: 1)
+                            .padding(.vertical, 4)
+                        }
+                        .listStyle(.plain)
+                    }
                 }
                 .padding(.top)
             }
             .navigationTitle("EV Station Finder")
-        }
+        }.alert(isPresented: $viewModel.showAlert) {
+            Alert(title: Text("Warning"), message: Text(viewModel.errorMessage ?? "Try again later"), dismissButton: .default(Text("OK"))) }
     }
 }
 
